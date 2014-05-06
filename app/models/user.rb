c52b90d3d884faaac7214f63119c94220addc7d4
Menisy <elmenisy@gmail.com>
2014-05-07 01:41:01 +0200

    fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
+  field :name
+  field :username
   ## Recoverable
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
